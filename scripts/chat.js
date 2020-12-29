583f29eaab56529eb7b0c90543392fe495b4beb0
jean-yves le cabellec <jean-yves.lecabellec@leetchi.com>
2020-12-29 23:20:45 +0100

    Create a class chatroom

---
+++ scripts/chat.js
@@ -0,0 +1,33 @@
+// adding new chat documents
+// setting up a real-time listener to get new chats
+// updating the username
+// updating the room
+
+class Chatroom {
+    constructor(room, username) {
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+    }
+
+    async addChat(message) {
+        // format a chat object
+        const now = new Date();
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+        // save the chat document
+        const response = await this.chats.add(chat);
+        return response;
+    }
+}
+
+const chatroom = new Chatroom('general', 'jean-yves');
+chatroom.addChat('I am trying another great message')
+    .then(() => console.log('chat added'))
+    .catch(err => console.log(err))
+
+console.log(chatroom);
