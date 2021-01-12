3637579f53b005307b917bce889e941c8a9d2ffb
jean-yves le cabellec <jean-yves.lecabellec@leetchi.com>
2021-01-12 09:54:53 +0100

    Add a new chat

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,19 @@
 // DOM queries
 const chatList = document.querySelector('.chat-list');
+const newChat = document.querySelector('.new-chat');
 
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('gaming', 'jean-yves');
+
+// Add new chat
+newChat.addEventListener('submit', e => {
+    e.preventDefault();
+    chatroom
+        .addChat(e.target.message.value.trim())
+        .then(() => newChat.reset())
+        .catch(error => console.log(error))    
+})
 
 // get chat and render
 chatroom.getChats(data => chatUI.render(data));
