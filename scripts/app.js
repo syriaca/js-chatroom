73ff17f835af3cc561547c619f61d864dd81ed94
jean-yves le cabellec <jean-yves.lecabellec@leetchi.com>
2021-01-13 16:44:37 +0100

    Added username change message feedback (Bootstrap 5 and JS)

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@
 const chatList = document.querySelector('.chat-list');
 const newChat = document.querySelector('.new-chat');
 const newName = document.querySelector('.new-name');
+const alertMessage = document.querySelector('.alert-message');
 
 // class instances
 const chatUI = new ChatUI(chatList);
@@ -19,8 +20,15 @@ newChat.addEventListener('submit', e => {
 // Change username
 newName.addEventListener('submit', e => {
     e.preventDefault();
-    chatroom.updateUsername(e.target.name.value.trim());
+    const newUserName = e.target.name.value.trim();
+    chatroom.updateUsername(newUserName);
     newName.reset();
+    alertMessage.innerHTML = `Ok username updated to <strong>${newUserName}</strong>`;
+    alertMessage.classList.remove('d-none');
+    setTimeout(() => {
+        alertMessage.classList.add('d-none');
+        alertMessage.innerHTML = ""
+    }, 3000);
 })
 
 // get chat and render
